e4a8980540e17fe8be08a2c3677bda2ab09e9fbc
Nathan Wurtz <nathan.wurtz01@gmail.com>
2021-12-05 13:45:48 +0100

    Create Poll.Data project, create .gitignore

---
+++ Poll.Data/DataModel/User.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Poll.Data
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public string Pseudo { get; set; }
+    }
+}
