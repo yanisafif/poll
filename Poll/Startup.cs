be6f79dd0d8bb9b923b83882b887f5b050781de4
Nathan Wurtz <nathan.wurtz01@gmail.com>
2021-12-09 21:04:28 +0100

    uncomment AddDbContext in Sartup

--- Poll/Startup.cs
+++ Poll/Startup.cs
@@ -26,14 +26,14 @@ namespace Poll
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            /*services.AddDbContext<AppDbContext>(dbCtxOption => {
+            services.AddDbContext<AppDbContext>(dbCtxOption => {
                 dbCtxOption.UseMySql(
                     Configuration.GetConnectionString("MariaDbConnectionString"),  
                     new MySqlServerVersion(new Version(Configuration["MysqlVersion"]))
                 ).LogTo(Console.WriteLine, LogLevel.Information)
                 .EnableSensitiveDataLogging()
                 .EnableDetailedErrors();
-            });*/
+            });
 
             services.AddScoped<IUsersRepository, UsersRepository>();
             services.AddScoped<IUsersService, UsersService>();
