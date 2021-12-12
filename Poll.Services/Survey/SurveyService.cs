f8d32c0b4f57dffedb8761301ca47774c801eaa8
Nathan Wurtz <nathan.wurtz01@gmail.com>
2021-12-12 15:54:26 +0100

    Create SurveyService and SurveyPreviewViewModel

---
+++ Poll.Services/Survey/SurveyService.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Poll.Services.ViewModel;
+using Poll.Data.Repositories;
+using Poll.Data.Model;
+
+namespace Poll.Services
+{
+    public class SurveyService : ISurveyService
+    {
+        private readonly ISurveyRepository _surveyRepo;
+
+        public async Task<IEnumerable<SurveyPreviewViewModel>> GetListPreview()
+        {
+            List<Survey> surveys = await this._surveyRepo.GetListAsync();
+
+            if(surveys is null || surveys.Count == 0)
+                return new List<SurveyPreviewViewModel>();
+
+            IEnumerable<SurveyPreviewViewModel> models = surveys.Select((a) => 
+                new SurveyPreviewViewModel()
+                {
+                    PollName = a.Name, 
+                    UserName = a.User.Pseudo, 
+                    CreationDate = a.CreationDate, 
+                    IsActive =  a.IsActive
+                }
+            );
+
+            return models ?? new List<SurveyPreviewViewModel>();
+        }
+    }
+}
---
+++ Poll.Services/ViewModel/SurveyPreviewViewModel.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Poll.Services.ViewModel
+{
+    public class SurveyPreviewViewModel
+    {
+        public string PollName { get; set; }
+        public string UserName { get; set; }
+        public DateTime CreationDate { get; set; }
+        public bool IsActive { get; set; }
+        
+    }
+}
